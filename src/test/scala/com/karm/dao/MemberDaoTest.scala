115363ebad64ebb2be0ce977bbcee7bf3a0747e3
jmhockham <jmhockham@gmail.com>
2018-02-26 13:17:27 +0000

    Added some more tests

--- src/test/scala/com/karm/dao/MemberDaoTest.scala
+++ src/test/scala/com/karm/dao/MemberDaoTest.scala
@@ -8,18 +8,25 @@ class MemberDaoTest extends FlatSpec with Matchers {
 
   DataInitialiser.init()
 
+  //we're going to use rough counts, as the live data can change as members come and go
   "Members table" should "have data" in {
     val membersCount = Database.query[Member].count()
-    membersCount>0 shouldBe true
+    membersCount>4000 shouldBe true
   }
 
   "getLords" should "get members in the lords" in {
     val members = MembersDao.getLords()
-    members.size shouldBe 2468
+    members.size>2000 shouldBe true
   }
 
   "getCommons" should "get members in the commons" in {
     val members = MembersDao.getCommons()
-    members.size shouldBe 1966
+    members.size>1500 shouldBe true
+  }
+
+  "Members" should "have a constituency" in {
+    val member = Database.query[Member].fetchOne().head
+    val constituencyOption = member.constituency
+    constituencyOption.nonEmpty shouldBe true
   }
 }
