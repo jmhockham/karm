9ad5e89bafa5582cdce2fafc5f869f5b4959c854
jmhockham <jmhockham@gmail.com>
2018-03-05 18:03:26 +0000

    Updated the terms download, so that it can recur through the web call results.

--- src/test/scala/com/karm/model/TermTest.scala
+++ src/test/scala/com/karm/model/TermTest.scala
@@ -3,6 +3,7 @@ package com.karm.model
 import java.io.File
 
 import com.karm.datafeed.DataFilesDownloader
+import com.karm.datafeed.DataFilesDownloader._
 import org.scalatest.{FlatSpec, Matchers}
 
 class TermTest extends FlatSpec with Matchers {
@@ -10,9 +11,9 @@ class TermTest extends FlatSpec with Matchers {
   private val memberJson = new File(getClass.getResource("/terms.json").getPath)
 
   "getTerms" should "return the json data" in {
-    val data = DataFilesDownloader.getTermsJson
+    val data = DataFilesDownloader.getTermsJson(TERMS_PAGE_LIMIT)
     data==null shouldBe false
-    data.length>1 shouldBe true
+    data.length shouldBe 5000
   }
 
 }
